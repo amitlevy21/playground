14908c9f1abb588019bec0b55048a3af1fe8d1b7
moshesheena <moshesheena@gmail.com>
2018-11-04 17:46:22 +0200

    Added classes for layout package
    Added class as defined in class to be
    used for creating controller classes

---
+++ src/main/java/com/sheena/playground/layout/NewUserForm.java
@@ -0,0 +1,54 @@
+package com.sheena.playground.layout;
+
+public class NewUserForm {
+	
+	private String email;
+	private String username;
+	private String avatar;
+	private String role;
+	
+	public NewUserForm(String email, String username, String avatar, String role) {
+		super();
+		this.email = email;
+		this.username = username;
+		this.avatar = avatar;
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "NewUserForm [email=" + email + ", username=" + username + ", avatar=" + avatar + ", role=" + role + "]";
+	}
+}
