e4f06d815aa7eb8cf81465250610c20576f5567e
moshesheena <moshesheena@gmail.com>
2018-11-05 22:12:11 +0200

    [WIP] Added REST controller class for 'users' resource
    Implemented methods for:
    - POST /playground/users
    - GET /playground/users/confirm/{playground}/{email}/{code}
    - GET /playground/users/login/{playground}/{email}
    - PUT /playground/users/{playground}/{email}
    
    Note: [WIP] = 'Work In Progress'

---
+++ src/main/java/com/sheena/playground/layout/UsersRestController.java
@@ -0,0 +1,57 @@
+package com.sheena.playground.layout;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersRestController {
+	//TODO: Add dependencies once there is a logic layer
+	private final String DUMMY_CODE = "code";
+	
+	@RequestMapping(
+			method=RequestMethod.POST,
+			path="/playground/users",
+			produces=MediaType.APPLICATION_JSON_VALUE,
+			consumes=MediaType.APPLICATION_JSON_VALUE)
+	public UserTO registerNewUser(@RequestBody NewUserForm newUserForm) {
+		//TODO: use newUserForm object to build a new user entity
+		return new UserTO("bison@flex.org", "playground", "username", "avatar", "player");
+	}
+	
+	@RequestMapping(
+			method=RequestMethod.GET,
+			path="/playground/users/confirm/{playground}/{email}/{code}",
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public UserTO verifyUserRegistration(
+			@PathVariable("email") String email, 
+			@PathVariable("code")String code) {
+		if (!this.DUMMY_CODE.equals(code)) {
+			//TODO: throw invalid code exception (maybe a custom class?)
+		}
+		return new UserTO(email, "playground", "username", "avatar", "player");
+	}
+	
+	@RequestMapping(
+			method=RequestMethod.GET,
+			path="/playground/users/login/{playground}/{email}",
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public UserTO userLoginRequest(
+			@PathVariable("playground") String playground, 
+			@PathVariable("email") String email) {
+		//TODO: apply logic once one exists? (0_0)
+		return new UserTO(email, playground, "username", "avatar", "player");
+	}
+	
+	@RequestMapping(
+			method=RequestMethod.PUT,
+			path="/playground/users/{playground}/{email}",
+			produces=MediaType.APPLICATION_JSON_VALUE,
+			consumes=MediaType.APPLICATION_JSON_VALUE)
+	public void updateUserProfile(@RequestBody UserTO userTO) {
+		//TODO: Right now there is no need to return something - once there is logic layer - rethink it
+	}
+}
