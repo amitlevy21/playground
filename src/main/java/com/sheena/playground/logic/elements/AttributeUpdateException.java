a27e8a452b3bc32a4a84587a3ff9d5579e186412
Amit Levy <amitlevy347@gmail.com>
2018-12-07 18:32:42 +0200

    remove todo stubs from exceptions

--- src/main/java/com/sheena/playground/logic/elements/AttributeUpdateException.java
+++ src/main/java/com/sheena/playground/logic/elements/AttributeUpdateException.java
@@ -4,29 +4,26 @@ import com.sheena.playground.logic.users.UsersException;
 
 public class AttributeUpdateException extends UsersException {
 
+	private static final long serialVersionUID = -1040556491719273659L;
+
 	public AttributeUpdateException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public AttributeUpdateException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public AttributeUpdateException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public AttributeUpdateException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public AttributeUpdateException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
--- src/main/java/com/sheena/playground/logic/users/RoleDoesNotExistException.java
+++ src/main/java/com/sheena/playground/logic/users/RoleDoesNotExistException.java
@@ -2,29 +2,26 @@ package com.sheena.playground.logic.users;
 
 public class RoleDoesNotExistException extends UsersException {
 
+	private static final long serialVersionUID = 5485956617160236025L;
+
 	public RoleDoesNotExistException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public RoleDoesNotExistException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RoleDoesNotExistException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RoleDoesNotExistException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RoleDoesNotExistException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
