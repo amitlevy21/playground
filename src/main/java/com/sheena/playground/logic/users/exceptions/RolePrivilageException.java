5fd536f9f61f6b62d76e6da6329afd8caced1caa
amitlevy21 <amitlevy347@gmail.com>
2018-12-23 20:21:04 +0200

    remove TODO stubs

--- src/main/java/com/sheena/playground/logic/users/exceptions/RolePrivilageException.java
+++ src/main/java/com/sheena/playground/logic/users/exceptions/RolePrivilageException.java
@@ -3,28 +3,23 @@ package com.sheena.playground.logic.users.exceptions;
 public class RolePrivilageException extends UsersException {
 
 	public RolePrivilageException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public RolePrivilageException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RolePrivilageException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RolePrivilageException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RolePrivilageException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
