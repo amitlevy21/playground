97b4f4f7db7c272dc3c8295568f79ad236c4a8be
itaytas <itaytas@gmail.com>
2018-12-26 20:44:21 +0200

    added check-in support - not tested yet

--- src/main/java/com/sheena/playground/plugins/CheckInPlugin.java
+++ src/main/java/com/sheena/playground/plugins/CheckInPlugin.java
@@ -8,30 +8,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sheena.playground.dal.ElementDao;
+import com.sheena.playground.dal.ActivityDao;
 import com.sheena.playground.logic.activities.ActivityEntity;
-
 
 @Component
 public class CheckInPlugin implements Plugin {
+	private final int minInHour = 60;
+
 	private ObjectMapper jackson;
-	private Date start;
-	private ElementDao elements;
-	
+	private Date currentTime;
+	private ActivityDao actvities;
+
 	@PostConstruct
 	public void init() {
 		this.jackson = new ObjectMapper();
-		
+		this.currentTime = new Date();
 	}
-	
+
 	@Autowired
-	public void setElements(ElementDao elements) {
-		this.elements = elements;
+	public void setElements(ActivityDao actvities) {
+		this.actvities = actvities;
 	}
 
 	@Override
 	public Object execute(ActivityEntity command) throws Exception {
-		return null;
+		boolean isValidDate = false;
+		String rvMessage;
+		PlayerStartWorking playerStartDate = jackson.readValue(command.getJsonAttributes(), PlayerStartWorking.class);
+
+		// getTime() returns the number of milliseconds since January 1, 1970, 00:00:00
+		// GMT represented by this Date object
+		long diff = this.currentTime.getTime() - playerStartDate.getStart().getTime();
+
+		// if diff < 0 --> playerStartDate is at the near future
+		// if diff = 0 --> playerStartDate is right now
+		// if diff > 0 --> playerStartDate is in the past, so we limit the diff
+		// to 1 hour == 60 min.
+		int diffmin = (int) (diff / (60 * 1000));
+
+		if (diffmin < 0) {
+			rvMessage = "Your start date is in the future - NOT VALID!";
+		} else if (diffmin > minInHour) {
+			rvMessage = "It's has been passed more than hour since your start date - NOT VALID!";
+		} else {
+			rvMessage = "Welcome, have a nice day!";
+			isValidDate = true;
+
+		}
+		command.getAttributes().put("validDate", isValidDate);
+		this.actvities.save(command);
+
+		return new PlayerActivityResponse(rvMessage);
 	}
 
 }
---
+++ src/main/java/com/sheena/playground/plugins/PlayerActivityResponse.java
@@ -0,0 +1,21 @@
+package com.sheena.playground.plugins;
+
+public class PlayerActivityResponse {
+	private String response;
+
+	public PlayerActivityResponse() {
+	}
+
+	public PlayerActivityResponse(String response) {
+		this.response = response;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+}
